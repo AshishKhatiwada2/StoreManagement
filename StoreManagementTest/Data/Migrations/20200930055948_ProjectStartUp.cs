3546c7acb0af9dad5802ed7ecf565fa267110476
ashish khatiwada <ashishkhatiwada212@gmail.com>
2020-09-30 11:45:17 +0545

    Add project files.

---
+++ StoreManagementTest/Data/Migrations/20200930055948_ProjectStartUp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StoreManagementTest.Data.Migrations
+{
+    public partial class ProjectStartUp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
